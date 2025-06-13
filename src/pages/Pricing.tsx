fa337072ee0737fb1fb0547fbc877deb6dcedbe5
brianhedden333 <brian@brianhedden.com>
2025-06-13 12:06:10 -0600

    Update Pricing.tsx

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,4 +1,5 @@
 
+
 import { Check } from 'lucide-react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -6,6 +7,7 @@ import VideoEmbed from '@/components/VideoEmbed';
 
 const Pricing = () => {
   const services = ['Planning and consultation with you, the wedding planner, and/or vendors', 'Free travel up to 50 miles from Boulder', 'Cinematic Aerial Footage', 'Video content options for before and/or after the ceremony', 'Support with livestreaming', 'Edited to your personalized soundtrack', 'Collaborative Editing Process', 'Convenient digital delivery and cloud storage'];
+  const services2 = ['Full Ceremony', '3-minute Highlight Edit', '12-minute Highlight Edit', 'Full Toast Video'];
   const portfolioImages = ['https://github.com/brianhedden333/lovebirdsweddingfilms/blob/main/public/lovable-uploads/Alexis.00_00_06_10.Still002.jpg?raw=true', 'https://github.com/brianhedden333/lovebirdsweddingfilms/blob/main/public/lovable-uploads/Arti%20Brandon%20Highlight%20FINAL.00_03_49_08.Still002.jpg?raw=true', 'https://github.com/brianhedden333/lovebirdsweddingfilms/blob/main/public/lovable-uploads/Erica%20and%20Jonathan%20Wedding%20Final.00_06_04_17.Still006.jpg?raw=true', 'https://github.com/brianhedden333/lovebirdsweddingfilms/blob/main/public/lovable-uploads/Austin.00_00_39_16.Still001%20copy.jpg?raw=true'];
   const nextSteps = ['Contact for custom quote based on your specific needs and vision', 'Contract signing and booking process with deposit', 'Pre-wedding consultation and timeline planning', 'Professional wedding day coverage with artistic storytelling', 'Post-production editing and delivery of final film(s)'];
   
@@ -47,6 +49,13 @@ const Pricing = () => {
               <h3 className="text-2xl font-serif text-gray-900 mb-8">Automatically Included</h3>
               <ul className="space-y-4">
                 {services.map((service, index) => <li key={index} className="flex items-start space-x-3">
+                    <Check className="w-5 h-5 text-teal-600 mt-1 flex-shrink-0" />
+                    <span className="text-gray-700">{service}</span>
+                  </li>)}
+              </ul>
+               <h3 className="text-2xl font-serif text-gray-900 mb-8">Custom Options</h3>
+              <ul className="space-y-4">
+                {services2.map((service, index) => <li key={index} className="flex items-start space-x-3">
                     <Check className="w-5 h-5 text-teal-600 mt-1 flex-shrink-0" />
                     <span className="text-gray-700">{service}</span>
                   </li>)}
